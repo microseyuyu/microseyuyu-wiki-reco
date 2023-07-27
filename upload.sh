7ebea2a3bf6bcc17bbd60aed0850dbef5b3a2321
microseyuyu <microseyuyu@outlook.jp>
2023-07-27 23:23:54 +0900

    no message

---
+++ upload.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -e
+
+echo "before run this script you should pull from upstream to avoid some unnecessary problem"
+read -p "Do you want procced?(y/n)" yn
+case $yn in
+        [yY] ) echo "ok,go on!";
+                    break;;
+        [nN] ) echo "exiting..."
+                    exit;;
+        *    ) echo "wrong input";
+esac
+
+echo "doing stuff"
+
+cd 
+
+read -p "please enter your currently project directory: " currently_directory
+read -p "And now please enter the directory which you want to upload: " upload_directory
+
+cd ~/$upload_directory
+
+
+cp -rf ~/$currently_directory/* ~/$upload_directory
+sudo rm -rf node_modules 
+
+echo "All done, please commit and push to upstream by yourself!"
